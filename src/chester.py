89c21dd1fd37d637fdab4322e637c9d364c805f6
mercuridi <kaibarberharris@protonmail.com>
2025-08-20 19:55:26 +0100

    set up logging

--- src/chester.py
+++ src/chester.py
@@ -1,4 +1,27 @@
 """Main code for Chester"""
+# first-party imports
+import logging
 
+# third-party imports
+import dotenv
+
+# global variables
 DISCORD_API_VERSION = 10
-DISCORD_API_ENDPOINT_BASE = f"https://discord.com/api/v{DISCORD_API_VERSION}"
+DISCORD_API_ENDPOINT_BASE_URL = f"https://discord.com/api/v{DISCORD_API_VERSION}"
+LOGGING_DESTINATION = "logs/chester.log"
+
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    filehandler = logging.FileHandler(
+        filename=LOGGING_DESTINATION,
+        mode="w",
+        encoding="utf8",
+    )
+    streamhandler = logging.StreamHandler()
+    logging.basicConfig(
+        handlers=[
+            filehandler, streamhandler
+        ],
+        level=logging.DEBUG
+    )
+    logging.info("Hello Logging!")
