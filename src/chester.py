9dd59986f3f397df19d7e48a10c824b12291af4c
mercuridi <kaibarberharris@protonmail.com>
2025-08-20 20:11:19 +0100

    this looks harder than i expected

--- src/chester.py
+++ src/chester.py
@@ -1,6 +1,7 @@
 """Main code for Chester"""
 # first-party imports
 import logging
+import requests
 
 # third-party imports
 import dotenv
@@ -9,6 +10,25 @@ import dotenv
 DISCORD_API_VERSION = 10
 DISCORD_API_ENDPOINT_BASE_URL = f"https://discord.com/api/v{DISCORD_API_VERSION}"
 LOGGING_DESTINATION = "logs/chester.log"
+
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run('token')
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
