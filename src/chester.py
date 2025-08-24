3004f1c2713d24ddbf7f1d99fdb5a8707deea890
mercuridi <kaibarberharris@protonmail.com>
2025-08-24 14:38:30 +0100

    oop refactor

--- src/chester.py
+++ src/chester.py
@@ -17,174 +17,146 @@ DISCORD_API_VERSION = 10
 DISCORD_API_ENDPOINT_BASE_URL = f"https://discord.com/api/v{DISCORD_API_VERSION}"
 LOGGING_DESTINATION = "logs/chester.log"
 
-# set up discord intents
-INTENTS = discord.Intents.default()
-INTENTS.message_content = True
+class Chester(commands.Bot):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-# set up bot prefix
-BOT = commands.Bot(command_prefix='>', intents=INTENTS)
+        # make handlers
+        filehandler = logging.FileHandler(
+            filename=LOGGING_DESTINATION,
+            mode="w",
+            encoding="utf8",
+        )
+        streamhandler = logging.StreamHandler()
 
-# set up global toggles
-# TODO find a better way to handle this
-LOOP = False
+        # set up logging with handlers
+        logging.basicConfig(
+            handlers=[
+                filehandler, streamhandler
+            ],
+            level=logging.DEBUG
+        )
+        self.loop_enabled = False
+
+    def hook(self, d):
+        if d["status"] == "downloading":
+            logging.info("%s %s", d['filename'], d['_percent_str'])
+        elif d["status"] == "finished":
+            logging.info("%s %s", d['filename'], d['_percent_str'])
+
+    @commands.command(name="loop")
+    async def cmd_loop(self, ctx):
+        # self.loop_enabled lives on your Bot instance
+        self.loop_enabled = not self.loop_enabled
+        status = "enabled" if self.loop_enabled else "disabled"
+        await ctx.send(f"{ctx.author.mention} Loop {status}")
 
 
-def hook(d):
-    if d["status"] == "downloading":
-        logging.info("%s %s", d['filename'], d['_percent_str'])
-    elif d["status"] == "finished":
-        logging.info("%s %s", d['filename'], d['_percent_str'])
-
-
-# bot commands
-@BOT.command()
-async def ping(ctx):
-    """basic example of creating a command and a reply"""
-    await ctx.send('pong')
-
-
-@BOT.command()
-async def download(ctx):
-    logging.info(ctx.message.content)
-    link = ctx.message.content.split()[1]
-    logging.info(link)
-    await ctx.send(f"{ctx.message.author.mention} Attempting to download track at URL `{link}`.")
-    try:
-        name = download_m4a(link)
-    except RuntimeError as e:
-        await ctx.send("An internal error occurred while downloading the file. Please contact the developer.")
-        raise RuntimeError(e) from e
-    await ctx.send(f"{ctx.message.author.mention} Successfully downloaded track `{name}`.")
-    logging.info(name)
-
-@BOT.command()
-async def resethard(ctx):
-    await ctx.send(f"{ctx.message.author.mention} Attempting to hard reset library...")
-    library_files = glob.glob('library/*')
-    metadata_files = glob.glob('metadata/*')
-    all_files = library_files + metadata_files
-    for file_path in all_files:
+    @commands.command(name="download")
+    async def cmd_download(self, ctx):
+        logging.info(ctx.message.content)
+        link = ctx.message.content.split()[1]
+        logging.info(link)
+        await ctx.send(f"{ctx.message.author.mention} Attempting to download track at URL `{link}`.")
         try:
-            os.remove(file_path)
-        except Exception as e:
-            logging.error("Failed to delete %s. Reason: %s", file_path, e)
-    await ctx.send(f"{ctx.message.author.mention} Hard reset complete")
+            name = self.download_m4a(link)
+        except RuntimeError as e:
+            await ctx.send("An internal error occurred while downloading the file. Please contact the developer.")
+            raise RuntimeError(e) from e
+        await ctx.send(f"{ctx.message.author.mention} Successfully downloaded track `{name}`.")
+        logging.info(name)
+
+    @commands.command(name="hardreset")
+    async def cmd_hardreset(self, ctx):
+        await ctx.send(f"{ctx.message.author.mention} Attempting to hard reset library...")
+        library_files = glob.glob('library/*')
+        metadata_files = glob.glob('metadata/*')
+        all_files = library_files + metadata_files
+        for file_path in all_files:
+            try:
+                os.remove(file_path)
+            except RuntimeError as e:
+                logging.error("Failed to delete %s. Reason: %s", file_path, e)
+        await ctx.send(f"{ctx.message.author.mention} Hard reset complete")
 
 
-def download_m4a(url):
-    ydl_opts = {
-        'format': 'm4a/bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'm4a',
-        }],
-        "outtmpl": 'library/%(id)s',
-        'noplaylist': True, # disables getting playlist
-        'progress_hooks': [hook], # adds the progress hook
-        'print_to_file': {
-            'video': [
-                # defines the json output format and destination
-                ('%(.{id,display_id,title,channel,upload_date,duration_string})#j',
-                'metadata/%(id)s.json')
-            ]
+    def download_m4a(self, url):
+        ydl_opts = {
+            'format': 'm4a/bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'm4a',
+            }],
+            "outtmpl": 'library/%(id)s',
+            'noplaylist': True, # disables getting playlist
+            'progress_hooks': [self.hook], # adds the progress hook
+            'print_to_file': {
+                'video': [
+                    # defines the json output format and destination
+                    ('%(.{id,display_id,title,channel,upload_date,duration_string})#j',
+                    'metadata/%(id)s.json')
+                ]
+            }
         }
-    }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-        ydl.process_info(info) # triggers download
-        return ydl.prepare_filename(info)
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            ydl.process_info(info) # triggers download
+            return ydl.prepare_filename(info)
 
-    logging.error("File download failed")
-    raise RuntimeError("A file download failed")
-
-@BOT.command()
-async def loop(ctx):
-    global LOOP
-    if LOOP is True:
-        LOOP = False
-        await ctx.send(f"{ctx.message.author.mention} Loop disabled")
-    else:
-        LOOP = True
-        await ctx.send(f"{ctx.message.author.mention} Loop enabled")
+        logging.error("File download failed")
+        raise RuntimeError("A file download failed")
 
 
-@BOT.command()
-async def stop(ctx):
-    voice = discord.utils.get(BOT.voice_clients, guild=ctx.guild)
-    if voice and voice.is_connected():
-        await voice.disconnect()
-        await ctx.send(f"{ctx.message.author.mention} Left the channel.")
-    else:
-        await ctx.send(f"{ctx.message.author.mention} There is no active track.")
+    @commands.command(name="stop")
+    async def cmd_stop(self, ctx):
+        voice = discord.utils.get(self.voice_clients, guild=ctx.guild)
+        if voice and voice.is_connected():
+            await voice.disconnect()
+            await ctx.send(f"{ctx.message.author.mention} Left the channel.")
+        else:
+            await ctx.send(f"{ctx.message.author.mention} There is no active track.")
 
 
-@BOT.command()
-async def play(ctx, *args):
-    try:
-        channel = ctx.message.author.voice.channel
-    except AttributeError as e:
-        logging.warning(e)
-        channel = None
+    @commands.command(name="play")
+    async def cmd_play(self, ctx, *args):
+        try:
+            channel = ctx.message.author.voice.channel
+        except AttributeError as e:
+            logging.warning(e)
+            channel = None
 
-    if not channel:
-        await ctx.send(f"{ctx.message.author.mention} You are not connected to a voice channel.")
-        return
+        if not channel:
+            await ctx.send(f"{ctx.message.author.mention} You are not connected to a voice channel.")
+            return
 
-    voice = discord.utils.get(BOT.voice_clients, guild=ctx.guild)
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
+        voice = discord.utils.get(self.voice_clients, guild=ctx.guild)
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
 
-    track_name = ' '.join(args)
-    source = discord.FFmpegPCMAudio(f"library/{track_name}.m4a")
-    if LOOP is True:
-        while LOOP:
+        track_name = ' '.join(args)
+        source = discord.FFmpegPCMAudio(f"library/{track_name}.m4a")
+        if self.loop_enabled is True:
+            while self.loop_enabled:
+                player = voice.play(source)
+                while not player.is_done():
+                    await asyncio.sleep(1)
+        else:
             player = voice.play(source)
             while not player.is_done():
                 await asyncio.sleep(1)
-    else:
-        player = voice.play(source)
-        while not player.is_done():
-            await asyncio.sleep(1)
 
-    # disconnect after the player has finished
-    player.stop()
-    await voice.disconnect()
-
-
-def start_bot():
-    # run the bot
-    logging.info("Starting discord bot")
-    BOT.run(os.environ.get("DISCORD_BOT_TOKEN"))
-    logging.info("Script finish")
-
-def main():
-    """driver code for chester"""
-
-    # load env
-    dotenv.load_dotenv()
-
-    # make handlers
-    filehandler = logging.FileHandler(
-        filename=LOGGING_DESTINATION,
-        mode="w",
-        encoding="utf8",
-    )
-    streamhandler = logging.StreamHandler()
-
-    # set up logging with handlers
-    logging.basicConfig(
-        handlers=[
-            filehandler, streamhandler
-        ],
-        level=logging.DEBUG
-    )
-
-    start_bot()
-    #logging.info(download_m4a('https://youtu.be/SPa8bPqQfmo'))
+        # disconnect after the player has finished
+        player.stop()
+        await voice.disconnect()
 
 if __name__ == "__main__":
     # call main function on direct run for script
-    main()
+    chester_intents = discord.Intents.default()
+    chester_intents.message_content = True
+    chester = Chester(command_prefix=">", intents=chester_intents)
+    # load env and run bot
+    dotenv.load_dotenv()
+    chester.run(os.environ.get("DISCORD_BOT_TOKEN"))
