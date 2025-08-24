0e0b00c46075c4158b7d384cff77819c4a4a6404
mercuridi <kaibarberharris@protonmail.com>
2025-08-24 19:11:33 +0100

    chester tech debt

--- src/chester.py
+++ src/chester.py
@@ -5,38 +5,40 @@
 import logging
 import os
 import asyncio
+from typing import Any
 
 # third-party imports
 import dotenv
 import discord
 from discord.ext import commands
 
-
 # global variables
 DISCORD_API_VERSION = 10
 DISCORD_API_ENDPOINT_BASE_URL = f"https://discord.com/api/v{DISCORD_API_VERSION}"
 LOGGING_DESTINATION = "logs/chester.log"
 
+
 class Chester(commands.Bot):
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: dict[str, Any]):
         super().__init__(**kwargs)
 
+
 async def main():
-    # ——— set up logging ———
+    # set up logging
     filehandler = logging.FileHandler(LOGGING_DESTINATION, mode="w", encoding="utf8")
     streamhandler = logging.StreamHandler()
     logging.basicConfig(handlers=[filehandler, streamhandler], level=logging.INFO)
 
-    # ——— create the bot ———
+    # create bot
     intents = discord.Intents.default()
     intents.message_content = True
     bot = Chester(command_prefix=">", intents=intents)
 
-    # ——— load cogs ———
+    # load cogs
     await bot.load_extension("musiccog")
     logging.info("registered commands: %s", [c.name for c in bot.commands])
 
-    # ——— start the bot ———
+    # start the bot
     token = os.environ["DISCORD_BOT_TOKEN"]
     await bot.start(token)
 
